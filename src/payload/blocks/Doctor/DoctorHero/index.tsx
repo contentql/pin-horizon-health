d3300debdff3268de587e9d972381a581ab20c20
jagadeesh <jagadeeshm778@gmail.com>
2024-07-12 13:13:44 +0530

    chore: doctors data fetched

--- src/payload/blocks/Doctor/DoctorHero/index.tsx
+++ src/payload/blocks/Doctor/DoctorHero/index.tsx
@@ -1,8 +1,14 @@
-import { DoctorHeroType, Media } from '@payload-types'
+'use client'
 
+import { Doctor, DoctorHeroType, Media } from '@payload-types'
+
+import Section from '@/components/common/Section'
+import DoctorListItem from '@/components/marketing/doctor/DoctorsListItems'
 import Hero from '@/components/marketing/doctor/Hero'
+import { trpc } from '@/trpc/client'
 
 function DoctorHero(doctorHero: DoctorHeroType) {
+  const { data: doctorsData } = trpc.doctor.getallDoctors.useQuery()
   return (
     <>
       <Hero
@@ -11,16 +17,16 @@ function DoctorHero(doctorHero: DoctorHeroType) {
         title={doctorHero?.title}
         subTitle={doctorHero?.sub_title}
       />
-      {/* <Section topMd={65} bottomMd={200} bottomLg={150} bottomXl={110}>
-        <TeamSectionStyle2 data={teamData} />
+      <Section topMd={65} bottomMd={200} bottomLg={150} bottomXl={110}>
+        <DoctorListItem doctorsData={doctorsData as Doctor[]} />
       </Section>
-      <Section className='cs_footer_margin_0'>
+      {/* <Section className='cs_footer_margin_0'>
         <BannerSectionStyle4
           bgUrl='/images/doctors/banner_bg_2.jpeg'
           title='Don’t Let Your Health <br />Take a Backseat!'
           subTitle='Schedule an appointment with one of our experienced <br />medical professionals today!'
         />
-      </Section> */}
+      </Section>  */}
     </>
   )
 }
--- src/trpc/routers/index.ts
+++ src/trpc/routers/index.ts
@@ -6,12 +6,15 @@ import { seedRouter } from '@/trpc/routers/seed'
 import { siteSettingsRouter } from '@/trpc/routers/site-settings'
 import { tagRouter } from '@/trpc/routers/tag'
 
+import { doctorRouter } from './doctor'
+
 export const appRouter = router({
   page: pageRouter,
   blog: blogRouter,
   siteSettings: siteSettingsRouter,
   tag: tagRouter,
   author: authorRouter,
+  doctor: doctorRouter,
   seed: seedRouter,
 })
 
