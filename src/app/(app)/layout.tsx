f7a59cdd9e0ab867d3a7d3e4dadeac4e7744997e
malla-lokesh <121810307046@gitam.in>
2024-07-22 10:09:52 +0530

    chore: added dynamic meta data feature

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,4 +1,6 @@
 import '../../theme/sass/index.scss'
+import configPromise from '@payload-config'
+import { getPayloadHMR } from '@payloadcms/next/utilities'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
@@ -7,9 +9,16 @@ import Provider from '@/trpc/Provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: 'Medical Horizon',
-  description: 'Your Partner in Health and Wellness',
+export async function generateMetadata(): Promise<Metadata> {
+  const payload = await getPayloadHMR({ config: configPromise })
+  const initData = await payload.findGlobal({
+    slug: 'site-settings',
+  })
+
+  return {
+    title: initData?.appName as string,
+    description: initData?.appDescription as string,
+  }
 }
 
 export default function RootLayout({
