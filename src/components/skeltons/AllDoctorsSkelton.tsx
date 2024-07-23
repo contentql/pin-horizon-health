8e113faec0e1331716e180885bc5e6e002fcc0d8
rahaman002 <36rahaman@gmail.com>
2024-07-23 17:38:44 +0530

    chore: fix skelton height

--- src/components/skeltons/AllDoctorsSkelton.tsx
+++ src/components/skeltons/AllDoctorsSkelton.tsx
@@ -3,7 +3,7 @@ const AllDoctorsSkelton = () => {
     <div className=' w-full'>
       <div className='cs_team cs_style_1 cs_type_2 cs_radius_20 h-[660px] w-full animate-pulse overflow-hidden text-center'>
         <div className='cs_member_img w-full'>
-          <div className='h-[450px] w-full bg-gray-300'></div>
+          <div className='h-[415px] w-full bg-gray-300'></div>
           <div className='cs_label cs_white_color cs_accent_bg mx-auto mt-4 h-8 w-32 bg-gray-300'></div>
         </div>
         <div className='cs_team_meta cs_white_bg h-[250px]'>
@@ -16,7 +16,7 @@ const AllDoctorsSkelton = () => {
               <div className='mx-auto h-4 w-5/6 rounded bg-gray-300'></div>
             </div>
           </div>
-          <div className='mt-4 space-y-3'>
+          <div className='mt-4  h-full w-full space-y-3'>
             <div className='mx-auto h-4 w-1/6 rounded bg-gray-300'></div>
             <div className='mx-auto h-4 w-1/6 rounded bg-gray-300'></div>
             <div className='mx-auto h-4 w-1/6 rounded bg-gray-300'></div>
