40d9e9b667b4069a29a689c1d95393efa7a44146
Rahaman <charanm927@gmail.com>
2024-07-26 16:02:25 +0530

    chore: description for contact page blocks

--- src/payload/blocks/Contact/ContactDetails/block.ts
+++ src/payload/blocks/Contact/ContactDetails/block.ts
@@ -13,6 +13,9 @@ export const ContactDetails_Block: Block = {
       name: 'title',
       label: 'Title',
       type: 'text',
+      admin: {
+        description: 'Enter title for contact details section',
+      },
     },
     {
       name: 'contact_info',
@@ -23,28 +26,47 @@ export const ContactDetails_Block: Block = {
           name: 'title',
           label: 'Title',
           type: 'text',
+          required: true,
+          admin: {
+            description:
+              'Enter title for contact details (Ex: Email, Location, Phone)',
+          },
         },
         {
           name: 'sub_title',
           label: 'Sub Title',
           type: 'text',
+          required: true,
+          admin: {
+            description: 'Enter your value for above title',
+          },
         },
         {
           name: 'contact_icon',
           label: 'Contact Icon',
+          required: true,
           type: 'select',
           options: [
             { label: 'Phone', value: '1' },
             { label: 'Stethoscope', value: '2' },
             { label: 'Location', value: '3' },
           ],
+          admin: {
+            description: 'Select icon for contact details',
+          },
         },
       ],
+      admin: {
+        description: 'Add your Contact details',
+      },
     },
     {
       name: 'location',
       label: 'Location',
       type: 'text',
+      admin: {
+        description: 'Enter your location to show on map',
+      },
     },
   ],
 }
