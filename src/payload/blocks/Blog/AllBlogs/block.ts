7750a561875610eb75777d9ea6e6863a358fe5cc
Rahaman <charanm927@gmail.com>
2024-07-26 16:01:43 +0530

    block: add description attribute

--- src/payload/blocks/Blog/AllBlogs/block.ts
+++ src/payload/blocks/Blog/AllBlogs/block.ts
@@ -13,6 +13,9 @@ export const AllBlogs_Block: Block = {
       name: 'title',
       label: 'Title',
       type: 'text',
+      admin: {
+        description: 'Enter title for Blogs section',
+      },
     },
   ],
 }
