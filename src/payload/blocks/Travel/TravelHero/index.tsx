309a67ac6077e6cf6ea83e190cfb87b009494819
Rahaman <charanm927@gmail.com>
2024-07-30 22:12:52 +0530

    chore: add contact form to travel page

--- src/payload/blocks/Travel/TravelHero/index.tsx
+++ src/payload/blocks/Travel/TravelHero/index.tsx
@@ -1,9 +1,7 @@
-import { DoctorHeroType, Media, Tour } from '@payload-types'
+import { DoctorHeroType, Media } from '@payload-types'
 
-import Section from '@/components/common/Section'
 import Hero from '@/components/marketing/doctor/Hero'
-import Spacing from '@/components/marketing/home/Spacing'
-import TravelItem from '@/components/marketing/travel/Traveltem'
+import TravelContactForm from '@/components/marketing/travel/TravelContactForm'
 import { trpc } from '@/trpc/client'
 
 function TravelHero(doctorHero: DoctorHeroType) {
@@ -16,16 +14,12 @@ function TravelHero(doctorHero: DoctorHeroType) {
         title={doctorHero?.title}
         subTitle={doctorHero?.sub_title}
       />
-      <div className='container'>
-        <Section topMd={65} bottomMd={200} bottomLg={150} bottomXl={110}>
-          <div className={`cs_team_grid cs_${'grid'}_view_wrap`}>
-            {tours?.map((tour, index) => (
-              <TravelItem tour={tour as Tour} key={index} />
-            ))}
+      <div className='cs_mt_minus_110 container pb-24'>
+        <div className='row'>
+          <div className='col-lg-10 offset-lg-1'>
+            <TravelContactForm />
           </div>
-          <Spacing md='110' lg='70' />
-          {/* <Pagination meta={yogaData?.meta} setPage={setPage} page={page} /> */}
-        </Section>
+        </div>
       </div>
     </>
   )
